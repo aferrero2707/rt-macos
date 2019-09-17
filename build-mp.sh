47ba30d11adac9fce157a06d1d4536142886a244
aferrero2707 <aferrero1975@gmail.com>
2019-09-17 16:18:14 +0200

    Update build-mp.sh

--- build-mp.sh
+++ build-mp.sh
@@ -4,7 +4,7 @@ wd=$(pwd)
 
 export BUILD_TYPE=Release
 #cmake_opt='-DPROC_TARGET_NUMBER="1" -DPROC_LABEL="generic processor"       -DCACHE_NAME_SUFFIX="5-dev"       -DCMAKE_C_COMPILER="clang"       -DCMAKE_CXX_COMPILER="clang++"       -DWITH_LTO="ON"    -DCMAKE_OSX_SYSROOT="/" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9"  -DLENSFUNDBDIR="./share/lensfun" -DCMAKE_CXX_FLAGS="-std=c++11"'
-cmake_opt='-DPROC_TARGET_NUMBER="1" -DPROC_LABEL="generic processor" -DCACHE_NAME_SUFFIX="5-dev" -DWITH_LTO="ON" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9"  -DLENSFUNDBDIR="./share/lensfun" -DCMAKE_CXX_FLAGS="-std=c++11"'
+cmake_opt='-DPROC_TARGET_NUMBER="1" -DPROC_LABEL="generic_processor" -DCACHE_NAME_SUFFIX="5-dev" -DWITH_LTO="ON" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9"  -DLENSFUNDBDIR="./share/lensfun" -DCMAKE_CXX_FLAGS="-std=c++11"'
 
 export PATH="/opt/local/bin:$PATH"
 export LD_LIBRARY_PATH="/opt/local/lib:$LD_LIBRARY_PATH"

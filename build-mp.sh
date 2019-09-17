d2784f10c7d12b39f365509b0dafd7e0c8bc0199
aferrero2707 <aferrero1975@gmail.com>
2019-09-17 13:21:29 +0200

    Added "make macosx_bundle" command

--- build-mp.sh
+++ build-mp.sh
@@ -2,7 +2,7 @@
 
 wd=$(pwd)
 
-export BUILD_TYPE=RelWithDebInfo
+export BUILD_TYPE=Release
 cmake_opt=""
 
 export PATH="/opt/local/bin:$PATH"
@@ -13,11 +13,6 @@ export PATH="$wd/inst/bin:/Applications/CMake.app/Contents/bin:$PATH"
 export LD_LIBRARY_PATH="$wd/inst/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="$wd/inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 
-# needs: 
-# gtk3 +quartz
-# gtkmm3 fftw-3-single libiptcdata lensfun lcms2 expat libtiff libjpeg libpng zlib gtk-osx-application-gtk3
-
-
 which cmake
 
 if [ ! -e RawTherapee ]; then
@@ -27,9 +22,10 @@ fi
 mkdir -p build
 cd build
 rm -f CMakeCache.txt
-cmake $cmake_opt -DCMAKE_BUILD_TYPE=${BUILD_TYPE}  ../RawTherapee
+cmake $cmake_opt -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ../RawTherapee
 #make VERBOSE=1
 make -j 3
+make macosx_bundle
 
 cd ..
 

c17592abd6ad34f5cc49f00db0ed42c92ab61da0
aferrero2707 <aferrero1975@gmail.com>
2019-09-18 15:09:25 +0200

    Update build-mp.sh

--- build-mp.sh
+++ build-mp.sh
@@ -28,7 +28,7 @@ cd build
 rm -f CMakeCache.txt
 cmake -DPROC_TARGET_NUMBER="1" -DPROC_LABEL="generic_processor" -DCACHE_NAME_SUFFIX="5-dev" \
   -DCMAKE_C_COMPILER=clang-mp-8.0 -DCMAKE_CXX_COMPILER=clang++-mp-8.0 \
-  -DWITH_LTO="ON" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9"  -DLENSFUNDBDIR=./share/lensfun \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9"  -DLENSFUNDBDIR=./share/lensfun \
   -DCMAKE_CXX_FLAGS="-Wno-deprecated-register -std=c++11" -DCMAKE_BUILD_TYPE=${BUILD_TYPE}  ../RawTherapee || exit 1
 #make VERBOSE=1 
 make VERBOSE=1 -j 3 || exit 1
